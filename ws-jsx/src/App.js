80f321789c8c93f5439a313386a5057673363362
Bacembougda <89839441+BacemBG@users.noreply.github.com>
2021-11-04 15:23:04 +0000

    first commit

---
+++ ws-jsx/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Comments from './Components/Comments';
+import MainVideo from "./Components/MainVideo"
+import SuggestionVideos from './Components/SuggestionVideos/SuggestionVideos.js';
+
+function App() {
+  return (
+    <div className="container-fluid">
+    <div className="row mt-3">    
+      <MainVideo />
+      <SuggestionVideos />
+
+    </div>
+     <div>
+       <Comments />
+     </div>
+    </div>
+
+  );
+}
+
+export default App;
